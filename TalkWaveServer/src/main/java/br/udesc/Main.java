2dbab0534718872c0db9ef61c005dda38afc9877
Guilherme Mendes Rosa <guilhermemr.jlle@gmail.com>
2024-04-21 21:04:24 -0300

    .

--- TalkWaveServer/src/main/java/br/udesc/Main.java
+++
@@ -1,8 +0,0 @@
-package br.udesc;
-
-import br.udesc.model.ConnectionPool;
-
-import java.io.IOException;
-
-public class Main {
-}
--- TalkWaveServer/src/main/java/br/udesc/model/ConnectionPool.java
+++ TalkWaveServer/src/main/java/br/udesc/model/ConnectionPool.java
@@ -17,10 +17,13 @@ public class ConnectionPool {
     public void startServer(int port) throws IOException {
         this.server = new ServerSocket(port);
         this.users = new TreeSet<User>();
+        acceptSockets();
+    }
+
+    private void acceptSockets() throws IOException {
         while (true) {
             Socket clientSocket = server.accept();
-            new Thread(() -> {
-
+            Thread receiveThread = new Thread(() -> {
                 System.out.println("Cliente conectado: " + clientSocket);
                 Scanner entrada = null;
                 try {
@@ -30,31 +33,27 @@ public class ConnectionPool {
                 }
 
                 String userName = entrada.nextLine();
-
                 this.users.add(new User(userName, clientSocket));
-                System.out.println(userName);
-                System.out.println(this.users.size());
-                while (true) {
-                    if (entrada.hasNextLine()) {
-                        Message message = new Gson().fromJson(entrada.nextLine(), Message.class);
-                        User recipient = this.users.stream().filter(u -> u.getName().equals(message.getRecipient())).findFirst().get();
-                        Socket recipientSocket = recipient.getSocket();
-                        try {
-                            var saida = new PrintStream(recipientSocket.getOutputStream());
-                            saida.printf("Mensagem de %s: %s%n", userName, entrada.nextLine());
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
 
+                while (true) {
+                    if (!entrada.hasNextLine()) {
+                        continue;
+                    }
+
+                    Message message = new Gson().fromJson(entrada.nextLine(), Message.class);
+                    User recipient = this.users.stream().filter(u -> u.getName().equals(message.getRecipient())).findFirst().get();
+                    Socket recipientSocket = recipient.getSocket();
+                    try {
+                        PrintStream saida = new PrintStream(recipientSocket.getOutputStream());
+                        saida.printf("Mensagem de %s: %s%n", userName, entrada.nextLine());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
                     }
                 }
-            }).start();
-        }
-    }
+            });
 
-    public static void main(String[] args) throws IOException {
-        ConnectionPool connectionPool = new ConnectionPool();
-        connectionPool.startServer(8080);
+            receiveThread.start();
+        }
     }
 
 }
