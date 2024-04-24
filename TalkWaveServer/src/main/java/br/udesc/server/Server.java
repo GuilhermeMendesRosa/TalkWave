00422eeaee1c5e2aa8ff18f106649cc551fe5cfa
Guilherme Mendes Rosa <guilhermemr.jlle@gmail.com>
2024-04-24 18:40:25 -0300

    usando set sincronizado ao invés de vector

--- TalkWaveServer/src/main/java/br/udesc/server/Server.java
+++ TalkWaveServer/src/main/java/br/udesc/server/Server.java
@@ -8,24 +8,26 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Collections;
 import java.util.Scanner;
-import java.util.Vector;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
 
     private volatile ServerSocket server;
-    private Vector<User> users;
+    private Set<User> users;
 
     public void startServer(int port) throws IOException {
         System.out.println("SERVIDOR INICIANDO");
         this.server = new ServerSocket(port);
-        this.users = new Vector<>();
-        this.acceptSockets();
+        this.users = Collections.synchronizedSet(new TreeSet<>());
+        this.acceptConnections();
     }
 
-    private void acceptSockets() throws IOException {
+    private void acceptConnections() throws IOException {
         ExecutorService threadPool = Executors.newCachedThreadPool();
         while (true) {
             Socket clientSocket = server.accept();
@@ -35,7 +37,7 @@ public class Server {
                     this.registerUser(input.nextLine(), clientSocket);
 
                     while (input.hasNextLine()) {
-                        this.processMessage(input);
+                        this.processMessage(input.nextLine());
                     }
                 } catch (Exception e) {
                     throw new RuntimeException(e);
@@ -49,8 +51,7 @@ public class Server {
         System.out.println("Usuário conectado: " + userName + " - Socket: " + clientSocket);
     }
 
-    private void processMessage(Scanner input) throws IOException {
-        String jsonMessage = input.nextLine();
+    private void processMessage(String jsonMessage) throws IOException {
         Message message = new Gson().fromJson(jsonMessage, Message.class);
         User recipient = this.users.stream().filter(u -> u.getName().equals(message.getRecipient())).findFirst().get();
         PrintStream output = new PrintStream(recipient.getSocket().getOutputStream());
