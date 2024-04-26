37b6a9f904687bdb294d21ad47e91dc6959fa65a
Guilherme Mendes Rosa <guilhermemr.jlle@gmail.com>
2024-04-26 10:29:44 -0300

    Criando Pair

---
+++ talk-wave-server/src/main/java/br/udesc/model/Pair.java
@@ -0,0 +1,35 @@
+package br.udesc.model;
+
+import java.util.Objects;
+
+public class Pair<A, B> {
+    private final A first;
+    private final B second;
+
+    public Pair(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public A getFirst() {
+        return first;
+    }
+
+    public B getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> that = (Pair<?, ?>) o;
+        return (Objects.equals(first, that.first) && Objects.equals(second, that.second)) ||
+                (Objects.equals(first, that.second) && Objects.equals(second, that.first));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second) + Objects.hash(second, first);
+    }
+}
